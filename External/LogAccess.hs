a2cd50f09fa16dd9206ec35c8897b73eeba8a47c
Friedrich von Never <friedrich@fornever.me>
2015-03-07 21:21:38 +0600

    Fix tests, enable External.LogAccess.

--- External/LogAccess.hs
+++ External/LogAccess.hs
@@ -1,7 +1,23 @@
-module External.LogAccess ({-getTodayLogUrl,-} getLogUrl) where
+module External.LogAccess (getLogUrl, getTodayLogUrl) where
 
 import Data.Text
-import Prelude
+import Data.Time.Clock
+import Data.Time.Calendar
+import Prelude hiding (concat)
 
-getLogUrl :: (Int, Int, Int) -> Text
-getLogUrl _ = ""
+logUrlPrefix :: Text
+logUrlPrefix = "http://0xd34df00d.me/logs/chat/codingteam@conference.jabber.ru"
+
+pad :: Show a => Int -> a -> Text
+pad p = justifyRight p '0' . pack . show
+
+getLogUrl :: (Integer, Int, Int) -> Text
+getLogUrl (year, month, day) = concat [logUrlPrefix, "/", pad 4 year, "/", pad 2 month, "/", pad 2 day, ".html"]
+
+getDate :: IO (Integer, Int, Int)
+getDate = getCurrentTime >>= return . toGregorian . utctDay
+
+getTodayLogUrl :: IO Text
+getTodayLogUrl = do
+    date <- getDate
+    return $ getLogUrl date
--- test/Handler/IndexSpec.hs
+++ test/Handler/IndexSpec.hs
@@ -9,19 +9,6 @@ spec = withApp $ do
         statusIs 200
         htmlAllContain "h1" "codingteam"
 
-        request $ do
-            setMethod "POST"
-            setUrl IndexR
-            addToken
-            fileByLabel "Choose a file" "test/Spec.hs" "text/plain" -- talk about self-reference
-            byLabel "What's on the file?" "Some Content"
-
-        statusIs 200
-        -- more debugging printBody
-        htmlCount ".message" 1
-        htmlAllContain ".message" "Some Content"
-        htmlAllContain ".message" "text/plain"
-
     -- This is a simple example of using a database access in a test.  The
     -- test will succeed for a fresh scaffolded site with an empty database,
     -- but will fail on an existing database with a non-empty user table.
