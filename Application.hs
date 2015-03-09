4b04e3aee03080d10113cdd307d61a0b4520b532
Friedrich von Never <friedrich@fornever.me>
2015-03-09 17:57:08 +0600

    Add Resources handler.

--- Application.hs
+++ Application.hs
@@ -31,7 +31,7 @@ import System.Log.FastLogger                (defaultBufSize, newStdoutLoggerSet,
 -- Import all relevant handler modules here.
 -- Don't forget to add new modules to your cabal file!
 import Handler.Common
-import Handler.Index
+import Handler.Home
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
