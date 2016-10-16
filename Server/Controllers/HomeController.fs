c1fe8715ff0d0fd3a4f814f5186abc5ae3b131af
Friedrich von Never <friedrich@fornever.me>
2016-10-16 19:52:12 +0700

    #34: general page logic

--- Server/Controllers/HomeController.fs
+++ Server/Controllers/HomeController.fs
@@ -1,9 +1,23 @@
 namespace Codingteam.Site.Controllers
 
+open System
+open System.Globalization
+
 open Microsoft.AspNetCore.Mvc
 
 type HomeController() =
     inherit Controller()
 
+    let logUrlPrefix = "http://0xd34df00d.me/logs/chat/codingteam@conference.jabber.ru"
+    let getLogUrl (date : DateTime) =
+        let dateString = date.ToString("yyyy/MM/dd", CultureInfo.InvariantCulture)
+        sprintf "%s/%s.html" logUrlPrefix dateString
+    let logTimezone = TimeSpan.FromHours 4.0
+    let getTodayLogUrl () = getLogUrl <| DateTime.UtcNow.Date + logTimezone
+
     member this.Index() =
+        this.ViewData.["LogUrl"] <- getTodayLogUrl ()
+        this.View()
+
+    member this.Resources() =
         this.View()
