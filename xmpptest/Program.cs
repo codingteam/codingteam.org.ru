681afafd60964ae6610578cce5a4539969b68d2b
Friedrich von Never <friedrich@fornever.me>
2016-12-08 23:47:30 +0700

    #27: add test code for SharpXMPP

---
+++ xmpptest/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Xml.Linq;
+using SharpXMPP;
+using SharpXMPP.XMPP;
+using SharpXMPP.XMPP.Client.MUC.Bookmarks.Elements;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var c = new XmppClient(new JID("yyyyyy@jabber.ru"), "xxxxxxx");
+            c.SignedIn += (_, __) => {
+                Console.WriteLine("Connected");
+                var room = new BookmarkedConference();
+                room.SetAttributeValue("jid", "codingteam@conference.jabber.ru");
+
+                var element = new XElement("storage:bookmarks", "nick");
+                element.Value = "hortolet";
+                room.Add(element);
+
+                c.BookmarkManager.Join(room);
+            };
+
+            c.Message += (s, e) => {
+                Console.Write("<-"); Console.WriteLine(e);
+            };
+
+            c.Element += (s, e) => {
+                Console.Write(e.IsInput ? "<-" : "->"); Console.WriteLine(e.Stanza);
+            };
+
+            Console.WriteLine("Connecting...");
+            c.Connect();
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
